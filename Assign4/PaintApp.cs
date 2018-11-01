4c160bb144e7bbb1743862e0e22e3b20ce18f043
tradesdr <tysabs@gmail.com>
2018-11-01 18:30:08 -0500

    Move Logic to Controller

--- Assign4/PaintApp.cs
+++ Assign4/PaintApp.cs
@@ -29,61 +29,6 @@ namespace Assign4
             Size = new Size(w, h);
         }
 
-        private void Box_Click(object sender, EventArgs e)
-        {
-            TextBox temp = sender as TextBox; 
 
-  //          pictureBox1.BackColor = temp.BackColor;
-            selectedColor = temp.BackColor; 
-
-        }
-
-        private void PaintCanvas_MouseDown(object sender, MouseEventArgs e)
-        {
-            if (point1.IsEmpty)
-            {
-                point1 = e.Location;
-            } else if (point2.IsEmpty)
-            {
-                point2 = e.Location;
-            }
-        }
-
-        private void PaintCanvas_MouseMove(object sender, MouseEventArgs e)
-        {
-
-        }
-        private void PaintCanvas_MouseUp(object sender, MouseEventArgs e)
-        {
-            if (!point1.IsEmpty && !point2.IsEmpty)
-            {
-                PaintCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.PaintCanvas_Paint);
-                PaintCanvas.Refresh();
-            }
-        }
-
-        private void PaintCanvas_Paint(object sender, PaintEventArgs e)
-        {
-
-            Pen linePen = new Pen(selectedColor);
-
-            if (!point1.IsEmpty && !point2.IsEmpty)
-            {
-
-                int x1 = point1.X;
-                int y1 = point1.Y;
-                int x2 = point2.X;
-                int y2 = point2.Y;
-
-                e.Graphics.DrawLine(linePen, x1, y1, x2, y2);
-
-                point1 = Point.Empty;
-                point2 = Point.Empty;
-
-
-            }
-
-    
-        }
     }
 }
